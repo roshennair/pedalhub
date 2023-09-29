9464bbfac4f2045bd228289a624fe254f7651e20
Roshen Nair <roshen620@gmail.com>
2023-09-29 12:39:02 +0800

    Initialized project and setup basic authentication

---
+++ PedalHub/Areas/Identity/Data/PedalHubUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PedalHub.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the PedalHubUser class
+public class PedalHubUser : IdentityUser
+{
+    [PersonalData]
+    public string FullName { get; set; }
+}
+
