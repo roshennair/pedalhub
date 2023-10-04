de511d60d4f6d45b933f8ad5b4122acd3c56f8ed
Arvind R <73293742+arvindnagarajan@users.noreply.github.com>
2023-10-05 04:12:43 +0800

    Added migration for rental table

---
+++ PedalHub/Migrations/20231004201126_AddedRentalTableFunctionality.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PedalHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRentalTableFunctionality : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
